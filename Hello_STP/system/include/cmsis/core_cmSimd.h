a2129bdcea205b65a229ef9bbec9b27b1cb406b7
Beshoy Ageeb Wesa <besho.ageeb88@gmail.com>
2024-03-29 17:23:53 +0200

    comment

--- Hello_STP/system/include/cmsis/core_cmSimd.h
+++ Hello_STP/system/include/cmsis/core_cmSimd.h
@@ -4,32 +4,15 @@
  * @version  V4.30
  * @date     20. October 2015
  ******************************************************************************/
-/* Copyright (c) 2009 - 2015 ARM LIMITED
 
-   All rights reserved.
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are met:
-   - Redistributions of source code must retain the above copyright
-     notice, this list of conditions and the following disclaimer.
-   - Redistributions in binary form must reproduce the above copyright
-     notice, this list of conditions and the following disclaimer in the
-     documentation and/or other materials provided with the distribution.
-   - Neither the name of ARM nor the names of its contributors may be used
-     to endorse or promote products derived from this software without
-     specific prior written permission.
-   *
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-   ARE DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS AND CONTRIBUTORS BE
-   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-   POSSIBILITY OF SUCH DAMAGE.
-   ---------------------------------------------------------------------------*/
+
+/* Copyright (c) 2009 - 2015 ARM LIMITED
+ * ...
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.
+ */
 
 
 #if   defined ( __ICCARM__ )
@@ -38,8 +21,10 @@
   #pragma clang system_header   /* treat file as system include file */
 #endif
 
+
 #ifndef __CORE_CMSIMD_H
 #define __CORE_CMSIMD_H
+
 
 #ifdef __cplusplus
  extern "C" {
@@ -48,9 +33,10 @@
 
 /* ###################  Compiler specific Intrinsics  ########################### */
 /** \defgroup CMSIS_SIMD_intrinsics CMSIS SIMD Intrinsics
-  Access to dedicated SIMD instructions
-  @{
-*/
+ *  Access to dedicated SIMD instructions
+ *  @{
+ */
+
 
 /*------------------ RealView Compiler -----------------*/
 #if   defined ( __CC_ARM )
@@ -86,11 +72,13 @@
 
 #endif
 
+
 /*@} end of group CMSIS_SIMD_intrinsics */
 
 
 #ifdef __cplusplus
 }
 #endif
+
 
 #endif /* __CORE_CMSIMD_H */
