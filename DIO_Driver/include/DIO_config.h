f4e444e41a1925c58137931ee78ecc3a7b83581a
Beshoy Ageeb Wesa <besho.ageeb88@gmail.com>
2024-03-29 17:23:03 +0200

    comment

--- DIO_Driver/include/DIO_config.h
+++ DIO_Driver/include/DIO_config.h
@@ -1,1 +1,8 @@
-/* Author  : BESHO						
+/* Author  : BESHO
+ * Filename: example.py
+ *  Date   : 2023-03-15
+ *  Purpose: This Python script demonstrates a simple program that calculates the factorial of a given number.
+ *  Input   : An integer n (n >= 0) representing the number for which the factorial will be calculated.
+ *  Output  : The factorial of the input number n.
+ */
+
