3616a9109bc76f92a21c7515d8658c1d459e4ef2
Beshoy Ageeb Wesa <besho.ageeb88@gmail.com>
2024-03-29 17:23:36 +0200

    comment

--- Hello_IR/include/EXTI0_config.h
+++ Hello_IR/include/EXTI0_config.h
@@ -1,28 +1,31 @@
-/*****************************************/
-/* Author  :  Ahmed Assaf                */
-/* Version :  V01                        */
-/* Date    :  29 August 2020             */
-/*****************************************/
-#ifndef EXTI0_CONFIG_H
+/*****************************************
+*  Author  :  Ahmed Assaf                *
+*  Version :  V01                        *
+*  Date    :  29 August 2020             *
+*****************************************/
+
+#ifndef EXTI0_CONFIG_H     /* Include guard to prevent multiple inclusions */
 #define EXTI0_CONFIG_H
 
 /*
-EXTI_LINE0
-EXTI_LINE1
-EXTI_LINE2
-.
-.
-.
-
+*  EXTI_LINE0
+*  EXTI_LINE1
+*  EXTI_LINE2
+*  .
+*  .
+*  .
 */
 
+/* Define the EXTI line to be configured */
 #define	EXTI_LINE			EXTI_LINE0
+
 /*
-FALLING_EDGE
-RISING_EDGE
-ON_CHANGE
+*  FALLING_EDGE
+*  RISING_EDGE
+*  ON_CHANGE
 */
+
+/* Define the sense mode for the EXTI line */
 #define	EXTI_SENSE_MODE		FALLING_EDGE
 
-
-#endif
+#endif /* EXTI0_CONFIG_H */
